e8142c9bbdb17b113ff8273da6641c03033ddb1c
redin <myobsidian@163.com>
2025-06-08 17:25:53 +0800

    Site updated: 2025-06-08 17:25:51

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749374355530";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749374750253";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
